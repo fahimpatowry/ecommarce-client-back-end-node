b18d9488582ae8769b091d0e8d916e79a122f357
@fahim.patowary <fahim.patowary@technonext.com>
2026-04-04 12:45:26 +0600

    add authentication middleware to protect routes with token verification

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt";
+
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
+export const protect = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1]; 
+
+        if (!token) {
+            return res.status(401).json({ message: "No token provided" });
+        }
+
+        const decoded = verifyToken(token);
+        req.user = decoded;
+
+        next();
+    }catch (error) {
+        res.status(401).json({
+            success: false,
+            message: "Unauthorized"
+        });
+    }
+}
+
