6da5b13e77b5d7a5af396fb07a2bbf8830049823
mdfahimpatowry <fahimjoy768@gmail.com>
2026-04-26 02:01:10 +0600

    enhance user profile routes with detailed request body schema for updates

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,18 +1,17 @@
 import { getUserProfile, updateUserProfile } from "../controllers/user.router";
 
-
 const route = require("express").Router();
 
 /**
  * @swagger
  *  /user:
- *  get: 
+ *  get:
  *     summary: Get user profile
  *     tags: [User]
  *     security:
  *      - bearerAuth: []
  *     responses:
- *       201: 
+ *       201:
  *          description: User profile fetched successfully
  */
 route.get("/", getUserProfile);
@@ -20,13 +19,42 @@ route.get("/", getUserProfile);
 /**
  * @swagger
  *  /user:
- *  put: 
+ *  put:
  *     summary: update user profile
  *     tags: [User]
  *     security:
  *      - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json:
+ *              schema:
+ *               type: object
+ *               properties:
+ *                name:
+ *                  type: string
+ *                  description: User's name
+ *                email:
+ *                  type: string
+ *                  description: User's
+ *                phone:
+ *                   type: string
+ *                   description: User's phone number (11 digits, starts with 01)
+ *                address:
+ *                   type: string
+ *                   description: User's address
+ *                gender:
+ *                   type: string
+ *                   description: "Male / Female"
+ *                role:
+ *                   type: string
+ *                   description: "User role (cr, admin, user)"
+ *               required:
+ *                - email
+ *                - name
+ *                - role
  *     responses:
- *       201: 
+ *       201:
  *          description: User profile update successfully
  */
 route.put("/", updateUserProfile);
